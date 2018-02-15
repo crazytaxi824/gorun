4e0f54d60f2ed8a324f708ba3108723e01074e01
crazytaxi <rayzc@outlook.com>
2018-02-15 21:25:03 +0800

    update FindRoot func fix os.exit(1) issue

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -23,7 +23,7 @@ func main() {
 
 		// 不用手动杀死进程，ctrl+c 终止主进程时同时会终止子进程
 
-		// 杀掉子进程
+		// 杀掉单独的子进程
 		// err := subProcess.Process.Kill()
 		// if err != nil {
 		// 	fmt.Println("项目进程退出失败:" + err.Error())
@@ -31,7 +31,7 @@ func main() {
 		// 	fmt.Println("项目进程退出成功")
 		// }
 
-		// 杀掉所有同名的进程
+		// 杀掉所有同名的所有进程
 		// cmdKillAll := exec.Command("killall", binFile)
 		// err := cmdKillAll.Run()
 		// if err != nil {
@@ -49,14 +49,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	// 寻找根路径
-	rootPath := FindRoot(curPath)
-
-	//判断是否在项目的根目录
-	findPath := rootPath + "/src/main/main.go"
-	_, err = os.Stat(findPath)
+	// 向上层文件夹寻找根路径
+	rootPath, err := FindRoot(curPath)
 	if err != nil {
-		fmt.Println(findPath+"不存在", err.Error())
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
@@ -70,10 +66,10 @@ func main() {
 
 	//获得当前路径的项目名
 	rootPaths := strings.Split(rootPath, "/")
-	binFile := rootPaths[len(rootPaths)-1] + "_.gorun"
+	gorunFile := rootPaths[len(rootPaths)-1] + "_.gorun"
 
 	fmt.Println("编译项目...")
-	cmd := exec.Command("go", "build", "-o", binFile, "./src/main/")
+	cmd := exec.Command("go", "build", "-o", gorunFile, "./src/main/")
 	cmd.Dir = rootPath
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -86,7 +82,7 @@ func main() {
 	fmt.Println("编译项目完成")
 	fmt.Println("~~~开始运行项目~~~ 以下开始为项目内的输出: ")
 	fmt.Println("")
-	subProcess = exec.Command("./" + binFile)
+	subProcess = exec.Command("./" + gorunFile)
 	subProcess.Dir = rootPath
 	subProcess.Stderr = os.Stderr
 	subProcess.Stdout = os.Stdout
@@ -98,25 +94,20 @@ func main() {
 }
 
 // FindRoot 用来找到项目根路径
-func FindRoot(path string) string {
+func FindRoot(path string) (string, error) {
 	var mark = false
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		fmt.Println("终端所在路径错误", err.Error())
-		os.Exit(1)
-	}
-
-	for _, v := range files {
-		if v.IsDir() && v.Name() == "src" {
-			mark = true
-			break
-		}
+	_, err := os.Stat(path + "/src/main/main.go")
+	if err == nil {
+		mark = true
 	}
 
 	if !mark {
 		index := strings.LastIndex(path, "/")
+		if index == -1 {
+			return "", errors.New("执行路径错误")
+		}
 		path := path[:index]
 		return FindRoot(path)
 	}
-	return path
+	return path, nil
 }
